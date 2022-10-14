8e873f08ea1e8d045abc2f0c035c8b6e0aad0e96
Alex <alexandre.zabelin@gmail.com>
2022-10-15 00:16:36 +0300

    feat: add PageNotFound component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes, BrowserRouter} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Persons from "./pages/Persons/Persons";
 import Nav from "./components/Nav/Nav";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/" element={<Nav />}>
                     <Route index element={<Home />} />
                     <Route path="main" element={<Persons />} />
+                    <Route path="*" element={<PageNotFound />} />
                 </Route>
 
             </Routes>
---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PageNotFound.css';
+
+const PageNotFound = () => {
+    return (
+        <div className="page-not-found">
+            <h3>404 Page not found</h3>
+            <p>Use only root (/) and main (/main) routes</p>
+        </div>
+    );
+};
+
+export default PageNotFound;
