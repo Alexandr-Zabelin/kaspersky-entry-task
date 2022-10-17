76b74e1f34791ccc3eda494f2e257041798fe35d
Alex <alexandre.zabelin@gmail.com>
2022-10-17 16:34:42 +0300

    feat: update colors and add responsive design

--- src/components/HeaderPanel/HeaderPanel.jsx
+++ src/components/HeaderPanel/HeaderPanel.jsx
@@ -7,17 +7,21 @@ import './HeaderPanel.css';
 const HeaderPanel = ({displayTypeOpt, searchOpt, selectOpt}) => {
     return (
         <div className="header-panel">
-            <DisplayType
-                onClick={displayTypeOpt.changeDisplayType}
-                currentValue={displayTypeOpt.displayType}/>
-            <SearchBar
-                placeholder={searchOpt.placeholder}
-                value={searchOpt.searchQuery}
-                onChange={searchOpt.changeSearchQuery} />
-            <Select
-                options={selectOpt.sortOptions}
-                labelText={selectOpt.label}
-                onChange={selectOpt.changeSortType} />
+            <div className="header-panel__search">
+                <SearchBar
+                    placeholder={searchOpt.placeholder}
+                    value={searchOpt.searchQuery}
+                    onChange={searchOpt.changeSearchQuery} />
+            </div>
+            <div className="header-panel__addition">
+                <DisplayType
+                    onClick={displayTypeOpt.changeDisplayType}
+                    currentValue={displayTypeOpt.displayType}/>
+                <Select
+                    options={selectOpt.sortOptions}
+                    labelText={selectOpt.label}
+                    onChange={selectOpt.changeSortType} />
+            </div>
         </div>
     );
 };
