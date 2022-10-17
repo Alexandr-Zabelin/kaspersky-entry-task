b01e4ac36839b1913cf5c943292a8765b27e0791
Alex <alexandre.zabelin@gmail.com>
2022-10-17 09:54:48 +0300

    feat: remove paragraph in PageNotFound component

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -5,7 +5,6 @@ const PageNotFound = () => {
     return (
         <div className="page-not-found">
             <h3>404 Page not found</h3>
-            <p>Use only root (/) and main (/main) routes</p>
         </div>
     );
 };
